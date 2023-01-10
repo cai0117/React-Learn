6308743e174535b9517e2658c2d4202d1d813359
caisiwei <1374632238@qq.com>
2023-01-10 18:07:27 +0800

    feat： useState

--- test/demo.js
+++ test/demo.js
@@ -1,30 +1,57 @@
+
+//指向当前正在执行的hooks
 let workInProgressHook;
+// 在 react 中，会通过判断 currentFiber 是否存在，来区别是 mount 还是 update
+// 在这个简易版本中，通过一个 全局变量来区分
 let isMount = true;
 
+
+// 函数组件有一个对应的 fiber
 const fiber = {
+
+   /**
+     * memorizedState 属性用于保存 hooks
+     * 是使用的 链表的结构来保存的hooks
+     */
   memoizedState: null,
   stateNode: App
 };
-
+//模拟React的调度
 function schedule() {
+  //初始化hooks为第一个hook
   workInProgressHook = fiber.memoizedState;
+
+  //模拟render阶段，render阶段会触发App函数
   const app = fiber.stateNode();
   isMount = false;
   return app;
 }
 
+
+// 作用：创建 update，并将 update 连成一条环状链表
+ // 这样我们在调用 useState 中，才能从 hook.queue.pending 中取到这条环状链表
 function dispatchAction(queue, action) {
   const update = {
     action,
     next:null
   }
   if (queue.pending === null) {
-    update.next = update;
+    update.next = update; // 第一个 update 会与自己形成 环状链表
   } else {
+
+    // 环形链表操作
+    // 例子： 3 -> 0 -> 1 -> 2 -> 3
+    // 转变为 4 -> 0 -> 1 -> 2 -> 3 -> 4  
+    
+    
+    // update 代表当前的 update，queue.pending.next 代表 第一个,
+    // 即实现 从 3 -> 0 变为 4 -> 0 
     update.next = queue.pending.next;
+
+    // 实现 从 -> 3 变为 3 -> 4   
     queue.pending.next = update;
   }
-  queue.pending = update;
+  queue.pending = update;  // queue.pending 指向的是最后一个 update 
 
   schedule();
 }
@@ -32,7 +59,8 @@ function dispatchAction(queue, action) {
 function useState(initialState) {
   let hook;
 
-  if (isMount) {
+  if (isMount) { //mount阶段
+    //创建hooks链表
     hook = {
       queue: {
         pending: null
@@ -40,29 +68,45 @@ function useState(initialState) {
       memoizedState: initialState,
       next: null
     }
+    // 将hook插入fiber.memoizedState链表末尾
     if (!fiber.memoizedState) {
       fiber.memoizedState = hook;
     } else {
       workInProgressHook.next = hook;
     }
-    workInProgressHook = hook;
+    // 移动workInProgressHook指针
+    workInProgressHook = hook; 
   } else {
+    // update时找到对应hook
     hook = workInProgressHook;
+    // 移动workInProgressHook指针
     workInProgressHook = workInProgressHook.next;
   }
 
+  // 此时 hook 变量就是当前的 hook 对象
+  // 省略了 state 优先级的考虑，所以只需要实现 state 中的 baseState
+  // 即baseState 就是 memorizedState
+
+  // update执行前的初始state
   let baseState = hook.memoizedState;
   if (hook.queue.pending) {
+    // 获取update环状单向链表中第一个update
     let firstUpdate = hook.queue.pending.next;
 
+
+    // 遍历链表
     do {
+      // 执行update action
       const action = firstUpdate.action;
       baseState = action(baseState);
       firstUpdate = firstUpdate.next;
     } while (firstUpdate !== hook.queue.pending)
 
-      hook.queue.pending = null;
+    // 清空queue.pending
+    hook.queue.pending = null;
   }
+
+  // 将update action执行完后的state作为memoizedState
   hook.memoizedState = baseState;
 
   return [baseState, dispatchAction.bind(null, hook.queue)];
@@ -81,5 +125,3 @@ function App() {
 }
 
 window.app = schedule();
-
-//window.app.click()
---
+++ test/reconciliction.js
@@ -0,0 +1,74 @@
+
+// function mountState<S>(
+//     initialState: (() => S) | S,
+//   ): [S, Dispatch<BasicStateAction<S>>] {
+//     const hook = mountWorkInProgressHook();
+//     if (typeof initialState === 'function') {
+//       // $FlowFixMe: Flow doesn't like mixed types
+//       initialState = initialState(); // 这里考虑了函数和普通值的两种情况
+//     }
+//     hook.memoizedState = hook.baseState = initialState; //赋初始值
+//     const queue: UpdateQueue<S, BasicStateAction<S>> = {
+//       pending: null,
+//       lanes: NoLanes, //调度优先级
+//       dispatch: null,
+//       lastRenderedReducer: basicStateReducer,
+//       lastRenderedState: (initialState: any), //处理特定情况下的优化
+//     };
+//     hook.queue = queue;
+//     const dispatch = (queue.dispatch = (dispatchSetState.bind(
+//       null,
+//       currentlyRenderingFiber,
+//       queue,
+//     ): any)) //绑定了当前fiber和queue的dispatchAction
+//     return [hook.memoizedState, dispatch];
+//   }
+  
+//   //其实，useState 就是一个预置了 reducer 的 useReducer，预置的 reducer 就是 basicStateReducer
+//   function updateState<S>(
+//     initialState: (() => S) | S,
+//   ): [S, Dispatch<BasicStateAction<S>>] {
+//     return updateReducer(basicStateReducer, (initialState: any));
+//   }
+  
+//   function rerenderState<S>(
+//     initialState: (() => S) | S,
+//   ): [S, Dispatch<BasicStateAction<S>>] {
+//     return rerenderReducer(basicStateReducer, (initialState: any));
+//   }
+
+
+//   function basicStateReducer<S>(state: S, action: BasicStateAction<S>): S {
+//     // $FlowFixMe: Flow doesn't like mixed types
+//     return typeof action === 'function' ? action(state) : action;
+//   }
+
+
+
+// /**
+//  * 创建一个 hook 对象
+//  * 若这是第一个 hook，挂载到 memoizedState
+//  * 若不是第一个hook的话，就会把他挂载到上一个 hook的 next 指针下，与上一个hook形成一条链表
+//  * 返回 该hook 对象
+//  */
+
+//   function mountWorkInProgressHook(): Hook {
+//     const hook: Hook = {  //创建一个Hook对象
+//       memoizedState: null,
+  
+//       baseState: null,
+//       baseQueue: null,  //baseState和baseQueue 指在存在优先级的情况下，通过update来计算state
+//       queue: null,
+  
+//       next: null,
+//     };
+  
+//     if (workInProgressHook === null) {
+//       // This is the first hook in the list
+//       currentlyRenderingFiber.memoizedState = workInProgressHook = hook; //第一个hook挂载到memoizedState
+//     } else {
+//       // Append to the end of the list
+//       workInProgressHook = workInProgressHook.next = hook; //不是第一个就挂载到next指针下，形成链表
+//     }
+//     return workInProgressHook;
+//   }
