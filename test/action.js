65a411a3638ec82a460487252f954284a23b687f
caisiwei <1374632238@qq.com>
2023-05-16 17:54:16 +0800

    feat：learn

--- test/action.js
+++ test/action.js
@@ -1,127 +1,30 @@
-let workInProgressHook;
+document.addEventListener("DOMContentLoaded", function () {
+  function createParagraph() {
+    let para = document.createElement("p");
+    para.textContent = "你点击了这个按钮";
+    document.body.appendChild(para);
+  }
 
+  const buttons = document.querySelectorAll("button");
 
-let isMount = true;
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", createParagraph);
+  }
+});
 
-
-
-
-
-
-const fiber={
-    memoizedState:null,
-
-    stateNode:App
-}
-
-function schedule(){
-
-    workInProgressHook = fiber.memoizedState;
-
-    const app = fiber.stateNode();
-
-    isMount = false;
-    return app;
-}
-const update = {
-    action,
-
-    next:null,
-}
-
-
-function dispatchAction(queue,action){
-    const update = {
-        action,
-    
-        next:null,
-    }
-
-    if(queue.pending === null){
-        queue.pending = update;
-    }else{
-        update.next = queue.pending.next;
-        queue.pending.next = update;
-    }
-
-    queue.pending = update;
-
-    schedule()
-}
-
-
-hook = {
-    queue:{
-        pending:null
-    },
-
-    memoizedState:initialState,
-
-    next:null
-}
-
-
-function useState(initialState){
-
-    let hook;
-
-    if(isMount){
-
-        hook = {
-            queue:{
-                pending:null
-            },
-        
-            memoizedState:initialState,
-        
-            next:null
-        }
-
-        if(!fiber.memoizedState){
-            fiber.memoizedState = hook;
-        }else{
-            workInProgressHook.next = hook;
-        }
-
-        workInProgressHook = hook;
-    }else{
-        hook = workInProgressHook;
-        workInProgressHook = workInProgressHook.next;
-    }
-
-
-    let baseState = hook.memoizedState;
-
-    if(hook.queue.pending){
-
-        let firstUpdate = hook.queue.pending.next;
-        do {
-            const action = firstUpdate.action;
-            baseState = action(baseState);
-            firstUpdate = firstUpdate.next;
-        }while (firstUpdate !== hook.queue.pending)
-
-        hook.queue.pending = null;
-    }
-
-    hook.memoizedState = baseState;
-
-    return [baseState,dispatchAction.bind(null, hook.queue)]
-
-
-}
-
-
-function App(){
-    const [num,updateNum] = useState(0);
-
-    console.log(`${isMount ? 'mount' : 'update'} num: `, num);
-
-  return {click() {
-    updateNum(num => num + 1);
-    updateNum(num => num + 1);
-    updateNum(num => num + 1);
-  }}
-}
-
-window.app = schedule();
+const changeBuffer = (file) => {
+  return new Promise((resolve) => {
+    let fileReader = new FileReader();
+    fileReader.readAsArrayBuffer(file);
+    fileReader.onload = (ev) => {
+      let buffer = ev.target.result,
+        spark = new SparkMD5.ArrayBuffer(),
+        HASH,
+        suffix;
+      spark.append(buffer);
+      HASH = spark.end();
+      suffix = /.([a-zA-Z0-9]+)$/.exec(file.name)[1];
+      resolve({ buffer, HASH, suffix, fileName: `${HASH}.${suffix}` });
+    };
+  });
+};
